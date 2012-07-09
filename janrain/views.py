332cbba492c1884c4e767b5cb50c15e2f69a4999
Carlo Costino <carlo.costino@wisertogether.com>
2012-07-09 18:02:39 -0400

    Cleaned up janrain view and fixed bug of the view trying to login you in even when your user is not authenticated.

--- janrain/views.py
+++ janrain/views.py
@@ -14,52 +14,74 @@ def login(request):
         token = request.POST['token']
     except KeyError:
         # TODO: set ERROR to something
-        login_failure.send(JanrainSignal, message='Error retreiving token', data=None)
+        login_failure.send(
+            JanrainSignal,
+            message='Error retreiving token',
+            data=None
+        )
+
         return HttpResponseRedirect('/')
 
     try:
-        profile = api.auth_info(token)
+        profile_data = api.auth_info(token)
     except api.JanrainAuthenticationError:
-        login_failure.send(JanrainSignal, message='Error retreiving profile', data=None)
+        login_failure.send(
+            JanrainSignal,
+            message='Error retreiving profile',
+            data=None
+        )
+
         return HttpResponseRedirect('/')
-    post_profile_data.send(JanrainSignal, profile_data=profile)
 
-    u = None
-    p = profile['profile']
+    post_profile_data.send(JanrainSignal, profile_data=profile_data)
 
-    u = auth.authenticate(profile=p)
-    post_authenticate.send(JanrainSignal, user=u, profile_data=profile)
+    user = None
+    profile = profile_data['profile']
 
-    juser = JanrainUser.objects.get_or_create(
-                user=u,
-                username=p.get('preferredUsername', ''),
-                provider=p.get('providerName', '').lower(),
-                identifier=p.get('identifier', ''),
-                avatar=p.get('photo', ''),
-                url=p.get('url', ''),
-            )[0]
-    juser.save()
-    post_janrain_user.send(JanrainSignal, janrain_user=juser, profile_data=profile)
+    user = auth.authenticate(profile=profile)
+    post_authenticate.send(JanrainSignal, user=user, profile_data=profile_data)
 
-    if u is not None:
-        request.user = u
-        auth.login(request, u)
-        post_login.send(JanrainSignal, user=u, profile_data=profile)
+    if user is not None:
+        janrain_user = JanrainUser.objects.get_or_create(
+            user=user,
+            username=profile.get('preferredUsername', ''),
+            provider=profile.get('providerName', '').lower(),
+            identifier=profile.get('identifier', ''),
+            avatar=profile.get('photo', ''),
+            url=profile.get('url', ''),
+        )[0]
+
+        janrain_user.save()
+        post_janrain_user.send(
+            JanrainSignal,
+            janrain_user=janrain_user,
+            profile_data=profile_data
+        )
+
+        request.user = user
+        auth.login(request, user)
+        post_login.send(JanrainSignal, user=user, profile_data=profile_data)
 
     try:
-        redirect = pre_redirect.send(JanrainSignal, type='login',
-                redirect=request.GET.get('next', '/'))[-1][1]
+        redirect = pre_redirect.send(
+            JanrainSignal,
+            type='login',
+            redirect=request.GET.get('next', '/')
+        )[-1][1]
     except IndexError:
         redirect = '/'
+
     return HttpResponseRedirect(redirect)
 
 
 def logout(request):
     pre_logout.send(JanrainSignal, request=request)
     auth.logout(request)
+
     try:
         redirect = pre_redirect.send(JanrainSignal, type='logout',
                 redirect=request.GET.get('next', '/'))[-1][1]
     except IndexError:
         redirect = '/'
+
     return HttpResponseRedirect(redirect)
