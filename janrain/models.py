b459f6d1344ec2c5c201596bd2623ea7ba134c95
Everett Gillert <everett.gillert@wisertogether.com>
2017-04-19 10:12:36 -0400

    Update model

--- janrain/models.py
+++ janrain/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 
 class JanrainUser(models.Model):
-    user       = models.ForeignKey(User, related_name='janrain_user')
+    user       = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='janrain_user')
     username   = models.CharField(max_length=512, blank=False)
     provider   = models.CharField(max_length=64, blank=False)
     identifier = models.URLField(max_length=512, blank=False)
