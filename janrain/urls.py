2a8c5c0810fbafaf8b33895e865609ff46c54f26
David Swenson <david.swenson@wisertogether.com>
2017-06-30 16:16:23 -0400

    Update url

--- janrain/urls.py
+++ janrain/urls.py
@@ -1,4 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+
 urlpatterns = [
-    (r'^login/$', 'login', name='janrain_login'),
-    (r'^logout/$', 'logout', name='janrain_logout'),
+    url(r'^login/$', views.login, name='janrain_login'),
+    url(r'^logout/$', views.logout, name='janrain_logout'),
 ]
