0f1237923b4c82daafb62d61987d5ae6ea0d5a71
David Swenson <david.swenson@wisertogether.com>
2018-02-02 14:28:57 -0500

    Update 0001_initial.py

--- janrain/migrations/0001_initial.py
+++ janrain/migrations/0001_initial.py
@@ -10,8 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('auth', '0008_alter_user_username_max_length'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL)
     ]
 
     operations = [
